0efc33841e775de5abdb624f0a502a4b9ee6722b
Matthew Giglio <matthew.giglio@duke.edu>
2022-03-29 22:34:36 -0400

    Added Coordinate class, commented out frontend API plans

--- doc/plan/api/frontend-external/BoardVisualizer.java
+++ doc/plan/api/frontend-external/BoardVisualizer.java
@@ -2,19 +2,20 @@
  * BoardVisualizer represents classes which display to the user a board that is able to hold Pieces.
  *
  * @author Edison Ooi
- */
+
 public interface BoardVisualizer {
 
   /**
    * Places a Piece of a certain type at the specified coordinates
    * @param coords Coordinates to place Piece at
    * @param type Type of piece being placed
-   */
+
   public void placePiece(Collection<Coordinate> coords, PieceType type);
 
   /**
    * Removes any Pieces that are at the coordinates contained in coords.
    * @param coords Coordinates that contain pieces to remove
-   */
+
   public void removePiece(Collection<Coordinate> coords);
 }
+*/
--- doc/plan/api/frontend-external/SetupBoard.java
+++ doc/plan/api/frontend-external/SetupBoard.java
@@ -3,20 +3,20 @@
  * users are able to place their Pieces on their board.
  *
  * @author Eric Xie, Edison Ooi, Minjun Kwak
- */
+
 public interface SetupBoard {
 
   /**
    * Places a piece on the player's board based on where the user clicked to place it.
    * @param coords The coordinates occupied by this piece
    * @param type The type of Piece that was placed
-   */
+
   void placePiece(List<Coordinate> coords, PieceType type);
 
   /**
    * Updates the visual indicator of the current Piece the user is placing.
    * @param coords The coordinates occupied by this Piece
-   */
+
   void updateCurrentPiece(List<Coordinate> coords);
 
   /**
@@ -24,8 +24,9 @@ public interface SetupBoard {
    * that the user made (ex: Ship placed out of bounds, invalid ship placement, etc.)
    *
    * @param errorMessage A string of the error message to be displayed to the user
-   */
+
   void displayError(String errorMessage);
 
 
 }
+ */
