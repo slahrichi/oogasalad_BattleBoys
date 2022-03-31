991d1271e373963daec2d63dfba3ba91476371d5
Matthew Knox <matthew.j.knox@duke.edu>
2022-03-30 21:59:32 -0400

    created AI Player

--- src/main/java/oogasalad/model/players/AIPlayer.java
+++ src/main/java/oogasalad/model/players/AIPlayer.java
@@ -1,2 +1,15 @@
-package oogasalad.model.players;public class AIPlayer {
+package oogasalad.model.players;
+
+import oogasalad.model.utilities.Board;
+
+public class AIPlayer extends GenericPlayer{
+
+  public AIPlayer(Board board) {
+    super(board);
+  }
+
+  @Override
+  public void playTurn() {
+
+  }
 }
