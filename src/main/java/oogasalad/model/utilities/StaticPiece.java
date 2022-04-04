e3f28bea96afdd1eec1cf7c836960334570e1a17
Luka Mdivani <lm378@duke.edu>
2022-04-04 13:16:20 -0400

    added id variable to piece

--- src/main/java/oogasalad/model/utilities/StaticPiece.java
+++ src/main/java/oogasalad/model/utilities/StaticPiece.java
@@ -1,27 +1,25 @@
 package oogasalad.model.utilities;
 
 import java.util.List;
-import oogasalad.model.utilities.tiles.Cell;
 import oogasalad.model.utilities.tiles.ShipCell;
 
 public class StaticPiece extends Piece {
-  public StaticPiece(List<ShipCell> cellList){
-    super(cellList);
+
+  public StaticPiece(List<ShipCell> cellList, String id) {
+    super(cellList, id);
   }
 
   @Override
   public void registerDamage(ShipCell hitLocation) {
 
-       if (getCellList().contains(hitLocation)) {
-         updateStatus("Damaged");
-         getCellList().remove(hitLocation);
-       }
-       if (checkDeath()) {
-         updateStatus("Dead");
-       }
-     }
-
-
+    if (getCellList().contains(hitLocation)) {
+      updateStatus("Damaged");
+      getCellList().remove(hitLocation);
+    }
+    if (checkDeath()) {
+      updateStatus("Dead");
+    }
+  }
 
 
 }
