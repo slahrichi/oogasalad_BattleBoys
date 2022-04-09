26dca3a9e7fd71553273740b984ad25945806f4b
Brandon Bae <brandon.bae@duke.edu>
2022-04-09 19:04:16 -0400

    created marker board class

--- src/main/java/oogasalad/model/utilities/tiles/enums/CellState.java
+++ src/main/java/oogasalad/model/utilities/tiles/enums/CellState.java
@@ -1,7 +1,7 @@
 package oogasalad.model.utilities.tiles.enums;
 
 public enum CellState {
-  NOT_DEFINED, WATER, SHIP_HEALTHY, SHIP_DAMAGED, SHIP_SUNKEN, ISLAND_HEALTHY, ISLAND_DAMAGED, ISLAND_SUNK;
+  NOT_DEFINED, WATER, WATER_HIT, SHIP_HEALTHY, SHIP_DAMAGED, SHIP_SUNKEN, ISLAND_HEALTHY, ISLAND_DAMAGED, ISLAND_SUNK;
 
   public static CellState of(int code) {
     if(code >= CellState.values().length) {
