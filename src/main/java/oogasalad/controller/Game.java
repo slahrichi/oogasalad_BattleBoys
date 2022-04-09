94607c112bc861e010f07ce621acd1ff9d50c808
Minjun Kwak <mk496@duke.edu>
2022-04-09 02:09:27 -0400

    Changed game to create the players, pieces, and board given the data parsed by the parser, and created a GameData record to be used by both game setup and game manager for game info

--- src/main/java/oogasalad/controller/Game.java
+++ src/main/java/oogasalad/controller/Game.java
@@ -3,30 +3,41 @@ package oogasalad.controller;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javafx.stage.Stage;
 import oogasalad.FilePicker;
+import oogasalad.GameData;
 import oogasalad.Parser;
 import oogasalad.PlayerData;
 import oogasalad.PropertyObservable;
 import oogasalad.controller.GameManager;
 import oogasalad.controller.GameSetup;
+import oogasalad.model.players.Player;
+import oogasalad.model.utilities.Board;
 import oogasalad.model.utilities.Coordinate;
 import oogasalad.model.utilities.Piece;
 import oogasalad.model.utilities.StaticPiece;
 import oogasalad.model.utilities.tiles.ShipCell;
 import oogasalad.model.utilities.tiles.enums.CellState;
 import oogasalad.view.GameView;
+import oogasalad.view.SetupView;
 
 public class Game extends PropertyObservable implements PropertyChangeListener {
+
+  private static final String FILEPATH = "oogasalad.model.players.";
 
   private GameSetup setup;
   private GameManager manager;
   private Stage myStage;
   private FilePicker fileChooser;
   private Parser parser;
+  private List<String> stringPlayers;
+  private GameData data;
 
   //TODO: Remove this variable, it's for testing only
   private List<Piece> pieceList = new ArrayList<>();
@@ -91,7 +102,12 @@ public class Game extends PropertyObservable implements PropertyChangeListener {
 //    pieceList.add(dummyShip3);
 //    pieceList.add(dummyShip4);
     //PlayerData data = parser.parse(chooseDataFile());
-    PlayerData data = new PlayerData(List.of("HumanPlayer", "HumanPlayer"), pieceList, dummyBoard);
+    stringPlayers = List.of("HumanPlayer", "HumanPlayer");
+    List<Player> players = new ArrayList<>();
+    for (int i = 0; i < stringPlayers.size(); i++) {
+      players.add(createPlayer(stringPlayers.get(i), dummyBoard, i));
+    }
+    data = new GameData(players, dummyBoard, pieceList);
     setup = new GameSetup(data);
     setup.addObserver(this);
     // GameManager should take in list of players and GameData
@@ -106,12 +122,35 @@ public class Game extends PropertyObservable implements PropertyChangeListener {
     myStage.show();
   }
 
+  private Player createPlayer(String playerType, CellState[][] board, int id) {
+    Board b = new Board(board);
+    Map<Integer, Board> enemyMap = createEnemyMap(b, id);
+    Player p = null;
+    try {
+      p = (Player) Class.forName(FILEPATH + playerType).getConstructor(Board.class, int.class,
+              Map.class)
+          .newInstance(b, id, enemyMap);
+    } catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
+        IllegalAccessException | NoSuchMethodException e) {
+    }
+    return p;
+  }
+
+  private Map<Integer, Board> createEnemyMap(Board b, int id) {
+    Map<Integer, Board> boardMap = new HashMap<>();
+    for (int i = 0; i < stringPlayers.size(); i++) {
+      if (i == id) continue;
+      boardMap.put(i, b.copyOf());
+    }
+    return boardMap;
+  }
+
   @Override
   public void propertyChange(PropertyChangeEvent evt) {
     System.out.println("Start game");
     //TODO: Change this to an instance of GameManager
-    GameView gameView = new GameView();
-    myStage.setScene(gameView.createScene());
-    gameView.updateShipsLeft(pieceList);
+    GameManager manager = new GameManager(data);
+    myStage.setScene(manager.createScene());
+    manager.updateShipsLeft(pieceList);
   }
 }
