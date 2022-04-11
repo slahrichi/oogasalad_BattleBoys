93075a654dd278c69afd227616075433a4187aaf
Luka Mdivani <lm378@duke.edu>
2022-04-10 22:39:25 -0400

    color selection added

---
+++ src/main/java/oogasalad/view/ColorSelectionStage.java
@@ -0,0 +1,62 @@
+package oogasalad.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class ColorSelectionStage {
+  private BorderPane myPane = new BorderPane();
+  private ResourceBundle myBuilderResources ;
+  private String[] optionList;
+  private List<Color> colorList=new ArrayList<>();
+  private List<ColorPicker> colorPickers=new ArrayList<>();
+  private Stage myStage;
+
+  public ColorSelectionStage(){
+    myStage =  new Stage();
+
+    myBuilderResources = ResourceBundle.getBundle("/BuilderInfo");
+    optionList= myBuilderResources.getString("possibleCellState").split(",");
+    myPane.setCenter(buildOptionDisplay());
+    Button continueButton = new Button("Continue");
+    continueButton.setOnAction(e->saveAndContinue());
+    myPane.setRight(continueButton);
+
+    Scene myScene =  new Scene(myPane);
+    myStage.setScene(myScene);
+    myStage.showAndWait();
+
+  }
+
+  private VBox buildOptionDisplay(){
+    VBox result = new VBox();
+    int index=0;
+    for(String option : optionList){
+      ColorPicker test = new ColorPicker();
+      colorPickers.add(test);
+      result.getChildren().add(new HBox(new Text(option),test));
+
+    }
+
+
+    return result;
+  }
+
+  private void saveAndContinue(){
+    for(ColorPicker cp : colorPickers){
+      colorList.add(cp.getValue());
+      System.out.println(cp.getValue().toString());
+    }
+    myStage.close();
+  }
+
+}
