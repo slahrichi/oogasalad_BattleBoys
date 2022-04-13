649834a9a38858b08249ec2a303cc629cc3b75c4
Matthew Knox <matthew.j.knox@duke.edu>
2022-04-12 22:43:34 -0400

    added decision engines to PlayerData

--- src/main/java/oogasalad/PlayerData.java
+++ src/main/java/oogasalad/PlayerData.java
@@ -5,7 +5,7 @@ import java.util.List;
 import oogasalad.model.utilities.Piece;
 import oogasalad.model.utilities.tiles.enums.CellState;
 
-public record PlayerData(List<String> players, List<Piece> pieces, CellState[][] board) {
+public record PlayerData(List<String> players, List<Piece> pieces, CellState[][] board, List<String> decisionEngines) {
 
   @Override
   public boolean equals(Object o) {
@@ -14,6 +14,7 @@ public record PlayerData(List<String> players, List<Piece> pieces, CellState[][]
     if(!(o instanceof PlayerData)) return false;
     PlayerData other = (PlayerData) o;
     if(!this.players.equals(other.players)) return false;
+    if(!this.decisionEngines.equals(other.decisionEngines)) return false;
     if(!this.pieces.equals(other.pieces)) return false;
     if(!Arrays.deepEquals(this.board, other.board)) return false;
     return true;
