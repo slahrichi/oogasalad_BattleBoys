4aa71e2dc1237e412a3bdee049b7a4e899c0b4f9
Matthew Giglio <matthew.giglio@duke.edu>
2022-04-15 16:15:09 -0400

    PlayerFactory refactored to create engines for AI players

--- src/main/java/oogasalad/controller/PlayerFactory.java
+++ src/main/java/oogasalad/controller/PlayerFactory.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import oogasalad.model.players.DecisionEngine;
-import oogasalad.model.players.EasyDecisionEngine;
 import oogasalad.model.players.Player;
 import oogasalad.model.utilities.Board;
 import oogasalad.model.utilities.MarkerBoard;
@@ -17,10 +16,13 @@ public class PlayerFactory {
 
   private static CellState[][] myBoard;
   private static int myRange;
-  private static int engineIndex;
   private static Map<Integer, DecisionEngine> engineMap;
+  private static List<String> myDifficulties;
   private static final String FILEPATH = "oogasalad.model.players.";
+  private static final String ENGINE = "DecisionEngine";
   private static final String INVALID_PLAYER = "Invalid player type given";
+  private static final String INVALID_DIFFICULTY = "Invalid difficulty given";
+
 
 
   public static PlayerFactoryRecord initializePlayers(CellState[][] board, List<String> playerTypes,
@@ -28,6 +30,7 @@ public class PlayerFactory {
     myBoard = board;
     myRange = playerTypes.size();
     engineMap = new HashMap<>();
+    myDifficulties = decisionEngines;
     List<Player> playerList = new ArrayList<>();
     for (int i = 0; i < playerTypes.size(); i++) {
       playerList.add(createPlayer(playerTypes.get(i), myBoard, i));
@@ -39,7 +42,7 @@ public class PlayerFactory {
     Board b = new Board(board);
     MarkerBoard mb = new MarkerBoard(board);
     Map<Integer, MarkerBoard> enemyMap = createEnemyMap(mb, id);
-    Player p = null;
+    Player p;
     try {
       p = (Player) Class.forName(FILEPATH + playerType).getConstructor(Board.class, int.class,
               Map.class)
@@ -52,12 +55,6 @@ public class PlayerFactory {
     return p;
   }
 
-  private static void createEngineIfAI(String playerType, int id, Map<Integer, MarkerBoard> enemyMap) {
-    if (playerType.equals("AIPlayer")) {
-      engineMap.put(id, new EasyDecisionEngine(null, enemyMap));
-    }
-  }
-
   private static Map<Integer, MarkerBoard> createEnemyMap(MarkerBoard mb, int id) {
     Map<Integer, MarkerBoard> boardMap = new TreeMap<>();
     for (int i = 0; i < myRange; i++) {
@@ -65,6 +62,21 @@ public class PlayerFactory {
       boardMap.put(i, mb.copyOf());
     }
     return boardMap;
+  }
+
+  private static void createEngineIfAI(String playerType, int id, Map<Integer, MarkerBoard> enemyMap) {
+    if (playerType.equals("AIPlayer")) {
+      try {
+        String difficulty = myDifficulties.get(id);
+        DecisionEngine ds = (DecisionEngine) Class.forName(FILEPATH + difficulty + ENGINE)
+            .getConstructor(List.class, Map.class).newInstance(null, enemyMap);
+        engineMap.put(id, ds);
+      }
+      catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
+          IllegalAccessException | NoSuchMethodException e) {
+        throw new NullPointerException(INVALID_DIFFICULTY);
+      }
+    }
   }
 
 }
