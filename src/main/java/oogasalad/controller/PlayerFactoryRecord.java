fb88c024eae6417df573ce4de679e8dbf4c40f35
Matthew Giglio <matthew.giglio@duke.edu>
2022-04-15 15:38:46 -0400

    Created PlayerFactoryRecord to hold results of PlayerFactory

---
+++ src/main/java/oogasalad/controller/PlayerFactoryRecord.java
@@ -0,0 +1,8 @@
+package oogasalad.controller;
+
+import java.util.List;
+import java.util.Map;
+import oogasalad.model.players.DecisionEngine;
+import oogasalad.model.players.Player;
+
+public record PlayerFactoryRecord(List<Player> playerList, Map<Integer, DecisionEngine> engineMap){}
