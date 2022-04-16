753032c689cba1542587f47215bedca1314c72d6
Matthew Giglio <matthew.giglio@duke.edu>
2022-04-16 13:00:55 -0400

    Initialized MediumDecisionEngine

---
+++ src/main/java/oogasalad/model/players/MediumDecisionEngine.java
@@ -0,0 +1,65 @@
+package oogasalad.model.players;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import oogasalad.model.utilities.Coordinate;
+import oogasalad.model.utilities.MarkerBoard;
+import oogasalad.model.utilities.tiles.enums.CellState;
+
+
+
+public class MediumDecisionEngine extends DecisionEngine {
+
+  private static final int[] ROW_DELTA = new int[]{-1, 0, 1, 1, -1, -1, 0, 1};
+  private static final int[] COL_DELTA = new int[]{-1, 1, 0, 1, 0, 1, -1, -1};
+
+  public MediumDecisionEngine(List<Coordinate> coordinateList, Map<Integer, MarkerBoard> enemyMap) {
+    super(coordinateList, enemyMap);
+  }
+
+
+  public EngineRecord makeMove() {
+    if (!getDeque().isEmpty()) {
+      return getDeque().pollFirst();
+    }
+    else {
+      int id = determineEnemy();
+      Coordinate location = determineLocation();
+      EngineRecord shot = new EngineRecord(location, id);
+      setLastShot(shot);
+      return shot;
+    }
+  }
+
+  private int determineEnemy() {
+    List<Integer> enemies = new ArrayList<>(getEnemyMap().keySet());
+    return enemies.get(getRandom().nextInt(enemies.size()));
+  }
+
+  private Coordinate determineLocation() {
+    return getCoordinateList().get(getRandom().nextInt(getCoordinateList().size()));
+  }
+
+
+  public void adjustStrategy(CellState result) {
+    if (wasSuccess(result)) {
+      prepareBFS();
+    }
+
+  }
+
+  private void prepareBFS() {
+    for (int i = 0; i < ROW_DELTA.length; i++) {
+      Coordinate c = new Coordinate(ROW_DELTA[i], COL_DELTA[i]);
+      if (getCoordinateList().contains(c)) {
+
+      }
+    }
+
+  }
+
+  private boolean wasSuccess(CellState result) {
+    return false;
+  }
+}
