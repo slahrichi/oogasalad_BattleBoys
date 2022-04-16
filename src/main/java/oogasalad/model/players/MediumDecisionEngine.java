253df2fad121bd32c4bfc3da33ae26848e1f8d39
Matthew Giglio <matthew.giglio@duke.edu>
2022-04-16 13:08:36 -0400

    Finished initial pass at MediumGameEngine

--- src/main/java/oogasalad/model/players/MediumDecisionEngine.java
+++ src/main/java/oogasalad/model/players/MediumDecisionEngine.java
@@ -8,16 +8,15 @@ import oogasalad.model.utilities.MarkerBoard;
 import oogasalad.model.utilities.tiles.enums.CellState;
 
 
-
 public class MediumDecisionEngine extends DecisionEngine {
 
   private static final int[] ROW_DELTA = new int[]{-1, 0, 1, 1, -1, -1, 0, 1};
   private static final int[] COL_DELTA = new int[]{-1, 1, 0, 1, 0, 1, -1, -1};
+  private int currentEnemyID;
 
   public MediumDecisionEngine(List<Coordinate> coordinateList, Map<Integer, MarkerBoard> enemyMap) {
     super(coordinateList, enemyMap);
   }
-
 
   public EngineRecord makeMove() {
     if (!getDeque().isEmpty()) {
@@ -25,6 +24,7 @@ public class MediumDecisionEngine extends DecisionEngine {
     }
     else {
       int id = determineEnemy();
+      currentEnemyID = id;
       Coordinate location = determineLocation();
       EngineRecord shot = new EngineRecord(location, id);
       setLastShot(shot);
@@ -44,22 +44,24 @@ public class MediumDecisionEngine extends DecisionEngine {
 
   public void adjustStrategy(CellState result) {
     if (wasSuccess(result)) {
+      getDeque().addFirst(getLastShot());
       prepareBFS();
     }
-
+    if (canBeRemoved(result)) {
+      getCoordinateList().remove(getLastShot());
+    }
   }
 
   private void prepareBFS() {
     for (int i = 0; i < ROW_DELTA.length; i++) {
       Coordinate c = new Coordinate(ROW_DELTA[i], COL_DELTA[i]);
       if (getCoordinateList().contains(c)) {
-
+        getDeque().addLast(new EngineRecord(c, currentEnemyID));
       }
     }
-
   }
 
   private boolean wasSuccess(CellState result) {
-    return false;
+    return result == CellState.ISLAND_DAMAGED || result == CellState.SHIP_DAMAGED;
   }
 }
