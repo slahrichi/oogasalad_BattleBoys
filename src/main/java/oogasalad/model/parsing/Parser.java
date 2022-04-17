350801365131feec0afeda8b2960e5bd3d651451
Matthew Knox <matthew.j.knox@duke.edu>
2022-04-17 19:17:31 -0400

    made class a bit cleaner

--- src/main/java/oogasalad/model/parsing/Parser.java
+++ src/main/java/oogasalad/model/parsing/Parser.java
@@ -1,29 +1,16 @@
 package oogasalad.model.parsing;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
 import oogasalad.PlayerData;
-import oogasalad.model.utilities.Piece;
-import oogasalad.model.utilities.tiles.enums.CellState;
 
 
 public class Parser {
@@ -56,7 +43,7 @@ public class Parser {
   private final String JSON_EXTENSION = "json";
   private final String JSON_ERROR = "jsonError";
   private final String BAD_PATH = "badPath";
-  //private final String REGEX = "[.][^.]+$";
+  private final String REGEX = "[.][^.]+$";
 
   private final String EMPTY = "";
   private final String SAVING_COMMENT = "generated via save";
@@ -70,12 +57,12 @@ public class Parser {
     File file = new File(pathToNewFile);
     Properties props = new Properties();
 
-    String nameOfNewFile = file.toString().replaceFirst("[.][^.]+$", EMPTY);
-    List<Object> items = PlayerData.getItems(data);
+    String nameOfNewFile = file.toString().replaceFirst(REGEX, EMPTY);
+    List<Object> elementsOfPlayerData = PlayerData.getItems(data);
 
     for(int i = 0; i < parsers.size(); i++) {
       ParsedElement parser = parsers.get(i);
-      parser.save(props, nameOfNewFile, items.get(i));
+      parser.save(props, nameOfNewFile, elementsOfPlayerData.get(i));
     }
     try {
       FileOutputStream outputStream = new FileOutputStream(file);
