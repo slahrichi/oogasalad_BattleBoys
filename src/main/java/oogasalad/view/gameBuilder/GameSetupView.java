b49ff9eef089d85a461446e98e900b72c891325f
Luka Mdivani <lm378@duke.edu>
2022-04-22 16:26:16 -0400

    working on filewriter

--- src/main/java/oogasalad/view/gameBuilder/GameSetupView.java
+++ src/main/java/oogasalad/view/gameBuilder/GameSetupView.java
@@ -1,7 +1,11 @@
 package oogasalad.view.gameBuilder;
 
+import java.lang.reflect.Method;
+import java.util.List;
 import javafx.application.Application;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import oogasalad.model.utilities.Usables.Weapons.BasicShot;
 
 public class GameSetupView extends Application{
 
@@ -13,7 +17,18 @@ public class GameSetupView extends Application{
   @Override
   public void start(Stage stage) throws Exception {
     Stage nstage= new Stage();
-    ColorSelectionStage b =new ColorSelectionStage();
-    BoardSetUpStage bb = new BoardSetUpStage();
+    //PieceDesignStage wds =new PieceDesignStage();
+    WeaponDesignStage wds = new WeaponDesignStage();
+    //PlayerSetupStage wds = new PlayerSetupStage();
+    //ColorSelectionStage b =new ColorSelectionStage();
+    //Class<?> clazz = Class.forName("oogasalad.view.gameBuilder.ColorSelectionStage");
+    //clazz.cast(b);
+    //Method testi = clazz.getDeclaredMethod("launch");
+    //System.out.println (clazz.getCanonicalName());
+    //System.out.println (testi.getDeclaringClass().getCanonicalName());
+    //testi.invoke(clazz);
+    Object t =wds.launch();
+    System.out.println(t.getClass());
+    //BoardSetUpStage bb = new BoardSetUpStage();
   }
 }
