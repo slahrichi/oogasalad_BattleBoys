ab87f4dd9d977767031bb0869ab7cca612ef94d6
Brandon Bae <brandon.bae@duke.edu>
2022-04-22 20:41:50 -0400

    added listener capability to shopitem and shopview

--- src/main/java/oogasalad/view/ShopItem.java
+++ src/main/java/oogasalad/view/ShopItem.java
@@ -1,27 +1,49 @@
 package oogasalad.view;
 
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import oogasalad.PropertyObservable;
+import oogasalad.view.maker.BoxMaker;
 import oogasalad.view.maker.ButtonMaker;
 
-public class ShopItem extends VBox {
+public class ShopItem extends PropertyObservable {
 
   private static final double WIDTH = 90;
   private static final double HEIGHT = 50;
   private static final double GAP = 20;
 
   private Rectangle item; //FIXME replace with ImageView later
-  private double price;
+  private int myPrice;
+  private VBox myVBox;
+  private String myName;
 
-  public ShopItem(String itemName, String itemInfo, int index) {
+  public ShopItem(String itemName, int Price, String usableClassName) {
+    myVBox = BoxMaker.makeVBox("VBoxId", 10, Pos.CENTER);
     item = new Rectangle(WIDTH,HEIGHT);
+    myPrice = Price;
+    myName = itemName;
+    /*
     this.setLayoutX((GAP + WIDTH) * (index % 3));
     this.setLayoutY((GAP + HEIGHT) * (index / 3));
-    this.getChildren().add(item);
-    this.getChildren()
-        .add(ButtonMaker.makeTextButton("Buy" + itemName, e -> applyItemProperties(), "Buy"));
+
+     */
+    myVBox.getChildren().add(item);
+    Button buyButton = ButtonMaker.makeTextButton("Buy" + itemName, e -> applyItemProperties(), "Buy");
+    myVBox.getChildren()
+        .add(buyButton);
+    buyButton.setOnAction(e -> buyItem());
+  }
+
+  private void buyItem() {
+    notifyObserver("buyItem", myName);
+  }
+
+  public VBox getMyVBox() {
+    return myVBox;
   }
 
   private void applyItemProperties() {
