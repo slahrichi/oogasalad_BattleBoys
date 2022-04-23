ed42b865fd80e19864d32a64d2c4ec62fb87323c
Matthew Giglio <matthew.giglio@duke.edu>
2022-04-23 17:03:31 -0400

    leveraged mockito to unit test the Game class

--- src/test/java/oogasalad/controller/GameTest.java
+++ src/test/java/oogasalad/controller/GameTest.java
@@ -1,12 +1,26 @@
 package oogasalad.controller;
 
+import java.beans.PropertyChangeEvent;
+import java.io.File;
+import java.util.ResourceBundle;
 import javafx.stage.Stage;
+import oogasalad.view.StartView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import util.DukeApplicationTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class GameTest extends DukeApplicationTest {
-  private Game game;
+
+private Game spy;
+
+  private final ResourceBundle myResources = ResourceBundle.getBundle("/languages/English");
+
 
   @BeforeEach
   void setup() {
@@ -14,8 +28,16 @@ public class GameTest extends DukeApplicationTest {
   }
 
   @Test
-  void testBasicGame() {
-    javafxRun(() -> game = new Game(new Stage()));
+  void testBasicGame() throws InterruptedException{
+    javafxRun(() ->
+    {
+      spy = Mockito.spy(new Game(new Stage()));
+      Mockito.doReturn(new File(System.getProperty("user.dir") + "/data/ExampleDataFile.properties")).when(spy).chooseDataFile();
+      spy.showStart();
+      spy.propertyChange(new PropertyChangeEvent(new StartView(myResources),
+          "loadFile", null, null));
+    });
+    Thread.sleep(3000);
+    assertNotEquals(null, lookup("#pass-computer-message-button").query());
   }
-
 }
