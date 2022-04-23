b2774c90a78c01d644277b43527f71c23cc41b57
Brandon Bae <brandon.bae@duke.edu>
2022-04-22 22:00:35 -0400

    added weapon icons + weapon image resource bundle

--- src/main/java/oogasalad/view/ShopItem.java
+++ src/main/java/oogasalad/view/ShopItem.java
@@ -1,8 +1,11 @@
 package oogasalad.view;
 
 
+import java.util.ResourceBundle;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -11,19 +14,25 @@ import oogasalad.view.maker.BoxMaker;
 import oogasalad.view.maker.ButtonMaker;
 
 public class ShopItem extends PropertyObservable {
+  private static final String DEFAULT_RESOURCE_PACKAGE = "/";
+  public static final String WEAPONS_IMAGES_PATH = "images/weapon_images/";
+  public static final String WEAPON_IMAGES_RESOURCE_BUNDLE = "WeaponImages.properties";
 
   private static final double WIDTH = 90;
   private static final double HEIGHT = 50;
   private static final double GAP = 20;
 
-  private Rectangle item; //FIXME replace with ImageView later
+  private ImageView item; //FIXME replace with ImageView later
   private int myPrice;
   private VBox myVBox;
   private String myName;
 
   public ShopItem(String itemName, int Price, String usableClassName) {
     myVBox = BoxMaker.makeVBox("VBoxId", 10, Pos.CENTER);
-    item = new Rectangle(WIDTH,HEIGHT);
+    //item = new Rectangle(WIDTH,HEIGHT);
+    ResourceBundle weaponImageBundle = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE+WEAPON_IMAGES_RESOURCE_BUNDLE);
+    String imageName = weaponImageBundle.getString(usableClassName);
+    item = new ImageView(new Image(DEFAULT_RESOURCE_PACKAGE+WEAPONS_IMAGES_PATH+imageName));
     myPrice = Price;
     myName = itemName;
     /*
