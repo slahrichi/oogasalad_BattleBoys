c977c5980930fa22d0a602c9758ff9609e6d306e
Prajwal Jagadish <Prajwal.Jagadish@duke.edu>
2022-04-24 22:18:18 -0400

    added new modifiers

---
+++ src/main/java/oogasalad/model/utilities/tiles/Modifiers/ReplaceWinCondition.java
@@ -0,0 +1,36 @@
+package oogasalad.model.utilities.tiles.Modifiers;
+
+import java.util.function.Consumer;
+import oogasalad.controller.GameManager;
+import oogasalad.model.players.Player;
+import oogasalad.model.utilities.winconditions.WinCondition;
+
+public class ReplaceWinCondition extends Modifiers {
+  WinCondition condition;
+
+  public ReplaceWinCondition(WinCondition condition) {
+    this.condition = condition;
+  }
+
+
+  @Override
+  protected Consumer createConsumer() {
+    Consumer ret = new GameManagerConsumer() {
+      @Override
+      public void accept(GameManager gameManager) {
+        gameManager.getConditionHandler().addWinCondition(condition);
+      }
+    };
+    return ret;
+  }
+
+  @Override
+  public String toString() {
+    return "Added " + condition.toString() + " as Win Condition";
+  }
+
+  @Override
+  public Consumer modifierFunction(GameManager gm) {
+    return myConsumer;
+  }
+}
---
+++ src/main/java/oogasalad/model/utilities/tiles/Modifiers/ScoreMultiplier.java
@@ -0,0 +1,2 @@
+package oogasalad.model.utilities.tiles.Modifiers;public class ScoreMultiplier {
+}
