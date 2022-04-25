c38794f1c538e0ccf6d1fce172d76e3813a6db26
Brandon Bae <brandon.bae@duke.edu>
2022-04-25 03:53:31 -0400

    final javadocs

--- src/main/java/oogasalad/model/utilities/MarkerBoard.java
+++ src/main/java/oogasalad/model/utilities/MarkerBoard.java
@@ -2,10 +2,20 @@ package oogasalad.model.utilities;
 
 import oogasalad.model.utilities.tiles.enums.CellState;
 
+/**
+ * MarkerBoard class is the backend model class used to keep track of shots each player have made against their opponent
+ * @author Brandon Bae
+ */
 public class MarkerBoard {
 
   private CellState[][] myMarkerArray;
 
+  /**
+   * Constructor for MarkerBoard Class
+   * Assumptions:
+   * - Expects only water or notdefined cell states
+   * @param cellStates 2d array of CellStates that represents the initial config of the markerboard
+   */
   public MarkerBoard(CellState[][] cellStates) {
     myMarkerArray = new CellState[cellStates.length][cellStates[0].length];
     for(int i = 0 ; i< myMarkerArray.length; i++) {
@@ -15,19 +25,27 @@ public class MarkerBoard {
     }
   }
 
+  /**
+   * Places the specified cellstate marker at the coordinate
+   * @param coord coordinate to place marker at
+   * @param marker cellstate representing result of the hit
+   */
   public void placeMarker(Coordinate coord, CellState marker) {
     myMarkerArray[coord.getRow()][coord.getColumn()] = marker;
   }
 
-  public boolean canPlaceAt(Coordinate coord) {
-    CellState coordState = myMarkerArray[coord.getRow()][coord.getColumn()];
-    return coordState != CellState.WATER_HIT && coordState != CellState.ISLAND_SUNK && coordState != CellState.SHIP_SUNKEN;
-  }
-
+  /**
+   * Make Copy of current MarkerBoard
+   * @return copy of current markerboard
+   */
   public MarkerBoard copyOf() {
     return new MarkerBoard(myMarkerArray);
   }
 
+  /**
+   * returns the 2d cellState array that makes up the marker board
+   * @return 2d cellState array myMarkerArray
+   */
   public CellState[][] getBoard() {
     return myMarkerArray;
   }
