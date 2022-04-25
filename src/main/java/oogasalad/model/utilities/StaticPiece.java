a835a7efc8ce68fcd6540770d0cbb596af095ad9
Brandon Bae <brandon.bae@duke.edu>
2022-04-24 20:55:35 -0400

    refactored piece class(es) based off design coach and removed useless methods

--- src/main/java/oogasalad/model/utilities/StaticPiece.java
+++ src/main/java/oogasalad/model/utilities/StaticPiece.java
@@ -15,20 +15,6 @@ public class StaticPiece extends Piece {
   public StaticPiece(List<ShipCell> cellList, List<Coordinate> relativeCoords, String id) {
     super(cellList, relativeCoords, NO_MOVEMENT_PATH, id);
   }
-  public StaticPiece(String id){
-    super(id);
-  }
-  @Override
-  public void registerDamage(ShipCell hitLocation) {
-
-    if (getCellList().contains(hitLocation)) {
-      updateStatus("Damaged");
-      getCellList().remove(hitLocation);
-    }
-    if (checkDeath()) {
-      updateStatus("Dead");
-    }
-  }
 
   @Override
   public Piece copyOf() {
